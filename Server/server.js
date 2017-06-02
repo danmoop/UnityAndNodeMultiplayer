26b7354d31a30279d07132b3b3dc1375c467e70a
danmoop <danmoople@gmail.com>
2017-06-02 23:53:48 +0300

    Add files via upload

--- Server/server.js
+++ Server/server.js
@@ -1,61 +1,61 @@
-var io = require('socket.io')(process.env.Port || 1337);
-var shortId = require('shortid');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
-console.log('server started');
+server.listen(process.env.PORT || 1337);
 
-var player = {
-    x: 0,
-    y: 0
+var users = [];
+
+function Player(x,y,id){
+    this.x = x;
+    this.y = y;
+    this.id = id;
 }
+
+console.log('Server Started(1)');
 
 io.on('connection', function(socket) {
 
+    var player = new Player(0,0,socket.id);
+    users.push(player);
 
-    socket.emit('spawn', player);
+    console.log(users);
+    socket.broadcast.emit('spawn', player);
 
-    console.log('connected');
-
-    socket.on('moveUpRequest', function() {
-    	player.y = player.y + 0.2;
-        socket.emit('setPos', player);
-        socket.broadcast.emit('setPos', player);
-        console.log(player);
-    });
-
-    socket.on('moveDownRequest', function() {
-    	player.y = player.y - 0.2;
-        socket.emit('setPos', player);
-        socket.broadcast.emit('setPos', player);
-        console.log(player);
-    });
-
-    socket.on('moveLeftRequest', function() {
-    	player.x = player.x - 0.2;
-        socket.emit('setPos', player);
-        socket.broadcast.emit('setPos', player);
-        console.log(player);
-    });
-
-    socket.on('moveRightRequest', function() {
-    	player.x = player.x + 0.2;
-        socket.emit('setPos', player);
-        socket.broadcast.emit('setPos', player);
-        console.log(player);
-    });
-
-    socket.on('clearChat', function(){
-    	for(var i = 0; i < 30; i++){
-    		console.log('');
-    	}
-    	console.log('------------------------------------------------------------------------');
-    	console.log('');
-    });
-
-    socket.on('showDebug', function(){
-        console.log(player);
-    });
+    for(var playerId in users){
+        if(playerId == socket.id)
+            continue;
+        socket.emit('spawn', users[playerId]);
+    }
 
     socket.on('disconnect', function() {
         console.log('disconnected');
+        socket.emit('close', player);
+        users.splice(player);
+    });
+
+    socket.on('moveUpRequest', function(){
+        player.y = player.y + 0.2;
+        socket.emit('setPos', player);
+        socket.broadcast.emit('setPos', player);
+    });
+
+    socket.on('moveDownRequest', function(){
+        player.y = player.y - 0.2;
+        socket.emit('setPos', player);
+        socket.broadcast.emit('setPos', player);
+    });
+
+    socket.on('moveLeftRequest', function(){
+        player.x = player.x - 0.2;
+        socket.emit('setPos', player);
+        socket.broadcast.emit('setPos', player);
+    });
+
+    socket.on('moveRightRequest', function(){
+        player.x = player.x + 0.2;
+        socket.emit('setPos', player);
+        socket.broadcast.emit('setPos', player);
     });
 });
